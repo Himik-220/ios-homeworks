81e2c6b6e61180c65720f339b93eb22daa131860
Himik220 <1703_ptz@mail.ru>
2022-11-20 02:46:04 +0300

    fixed ProfileView

--- Netology_IB_Instruments/ProfileViewController.swift
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -15,6 +15,7 @@ class ProfileViewController: UIViewController {
     let nib = UINib(nibName: "ProfileView", bundle: nil).instantiate(withOwner: self).first as! ProfileView
     nib.frame = UIScreen.main.bounds
     
+    
     self.view.addSubview(nib)
   }
 }
