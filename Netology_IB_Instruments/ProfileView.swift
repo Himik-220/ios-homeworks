490d31c648f27202740b27084c7e757c0ad91497
Himik220 <1703_ptz@mail.ru>
2022-11-20 02:31:05 +0300

    Initial Commit

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Leonid Shchipkov on 19.11.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+  @IBOutlet weak var name: UILabel!
+  @IBOutlet weak var birthDay: UILabel!
+  @IBOutlet weak var city: UILabel!
+  @IBOutlet weak var avatarImage: UIImageView!
+  @IBOutlet weak var signature: UITextView!
+}
